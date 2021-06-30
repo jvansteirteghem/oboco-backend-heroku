fceaa77a351b93e5da95f232d95fd2ae5b894c31
jeeto jeeto <jeeto.jeeto@gmail.com>
2021-06-30 13:59:31 +0200

    refactor

--- database_postgresql.sql
+++ database_postgresql.sql
@@ -1,4 +1,4 @@
-insert into bookCollections (directoryPath, name, normalizedName, updateDate, rootBookCollectionId, parentBookCollectionId, numberOfBookCollections, numberOfBooks, number) values ('', 'DEFAULT', 'default', current_timestamp, null, null, 0, 0, 1);
-insert into users (name, passwordHash, updateDate, rootBookCollectionId) values ('administrator', '$2a$12$msu32WtSMaQVCJsIDKCxkOTVOGRrncBjUe5x63GbY/RizCJ/zyFPC', current_timestamp, 1);
+insert into bookCollections (directoryPath, name, normalizedName, createDate, updateDate, rootBookCollectionId, parentBookCollectionId, numberOfBookCollections, numberOfBooks, number) values ('', 'DEFAULT', 'default', current_timestamp, current_timestamp, null, null, 0, 0, 1);
+insert into users (name, passwordHash, createDate, updateDate, rootBookCollectionId) values ('administrator', '$2a$12$msu32WtSMaQVCJsIDKCxkOTVOGRrncBjUe5x63GbY/RizCJ/zyFPC', current_timestamp, current_timestamp, 1);
 insert into userRoles (userId, role) values (1, 'ADMINISTRATOR');
 insert into userRoles (userId, role) values (1, 'USER');
